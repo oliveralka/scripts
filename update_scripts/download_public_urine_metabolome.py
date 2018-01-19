2de96665d608ad127e6515d1449435d6a0802f57
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-01-19 14:40:51 +0100

    [ADD] downoad_public_urine_metabolome now also formats concentrations

--- update_scripts/download_public_urine_metabolome.py
+++ update_scripts/download_public_urine_metabolome.py
@@ -7,8 +7,61 @@ import requests
 import zipfile
 import xml.etree.ElementTree as ET
 import csv
+import re
+
 
 def main(argv):
+
+    na_string = 'NA'
+    name_concentration_middle = 'concentration_middle'
+    name_concentration_lower = 'concentration_lower'
+    name_concentration_upper = 'concentration_upper'
+
+    def handle_uncertainty(m):
+        mean = float(m.group(1))
+        sd = float(m.group(2))
+        return (str(mean - sd), str(mean), str(mean + sd))
+
+    def handle_regular(m):
+        mean = float(m.group(1))
+        return (mean, mean, mean)
+
+    def handle_lower_bound(m):
+        return (m.group(1), 'NA', 'NA')
+
+    def handle_upper_bound(m):
+        return ('NA', 'NA', m.group(1))
+
+    def handle_upper_bound_exclusive_range(m):
+        return (m.group(1), 'NA', m.group(2))
+
+    re_handler = {
+        'uncertainty':
+        (re.compile(r'^([0-9]+(?:\.[0-9]+)?)\s*\+/-\s*([0-9]+(?:\.[0-9]+)?)$'),
+         handle_uncertainty),
+        'not_available':
+        (re.compile(r'^NA$'),
+         lambda x: (na_string, na_string, na_string)),
+        'regular':
+        (re.compile(r'^([0-9]+(?:\.[0-9]+)?)'),
+         handle_regular),
+        'lower_bound_exclusive':
+        (re.compile(r'^>\s*([0-9]+(?:\.[0-9]+)?)$'),
+         handle_lower_bound),  # >12.0
+        'upper_bound_exclusive':
+        (re.compile(r'^<\s*([0-9]+(?:\.[0-9]+)?)$'),
+         handle_upper_bound),  # < 14
+        'upper_bound_exclusive_range':
+        (re.compile(r'^<\s*([0-9]+(?:\.[0-9]+)?)\s*(?:-|–)\s*([0-9]+(?:\.[0-9]+)?)$'),
+         handle_upper_bound_exclusive_range),
+        'upper_bound_inclusive':
+        (re.compile(r'^<=\s*([0-9]+(?:\.[0-9]+)?)$'),
+         handle_upper_bound),
+        'upper_bound_exclusive_scientific':
+        (re.compile(r'^<([0-9]+(?:\.[0-9]+)?e-?[0-9]+)$'),
+         handle_upper_bound)   # <5.110e-05
+    }
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', type=str, required=True)
     parser.add_argument('--removetarget', action='store_true')
@@ -52,6 +105,9 @@ def main(argv):
     attributes_concentration = [
         'biofluid',
         'concentration_value',
+        name_concentration_lower,
+        name_concentration_middle,
+        name_concentration_upper,
         'concentration_units',
         'subject_age',
         'subject_sex',
@@ -65,6 +121,7 @@ def main(argv):
         if not value:
             return 'NA'
         return value
+
     def normalize(tag):
         tag = tag.replace(r'{http://www.hmdb.ca}', '')
 
@@ -85,11 +142,13 @@ def main(argv):
 
     with open(concentration_file, 'w') as outfile:
         outfile_csvwriter = csv.writer(outfile)
-        outfile_csvwriter.writerow(attributes_metabolites + attributes_concentration + ['is_normal_concentration'])
+        outfile_csvwriter.writerow(
+            attributes_metabolites + attributes_concentration +
+            ['is_normal_concentration'])
         for (event, elem) in parser_it:
             tag = normalize(elem.tag)
             if event == 'start':
-                if  tag == 'normal_concentrations':
+                if tag == 'normal_concentrations':
                     concentration_abnormal = False
                 elif tag == 'abnormal_concentrations':
                     concentration_abnormal = True
@@ -98,14 +157,14 @@ def main(argv):
                 if tag == 'metabolite':
 
                     # Commit concentration with all metabolites and reset
-                    row_metabolite = [ metabolite[x] if x in metabolite else 'NA' for x in attributes_metabolites ]
+                    row_metabolite = [metabolite[x] if x in metabolite else 'NA' for x in attributes_metabolites ]
                     for conc in normal_concentrations:
                         row_concentration = [ conc[x] if x in conc else 'NA' for x in attributes_concentration]
                         row_concentration.append(True)
                         outfile_csvwriter.writerow(row_metabolite + row_concentration)
 
                     for conc in abnormal_concentrations:
-                        row_concentration = [ conc[x] if x in conc else 'NA' for x in attributes_concentration]
+                        row_concentration = [conc[x] if x in conc else 'NA' for x in attributes_concentration]
                         row_concentration.append(False)
                         outfile_csvwriter.writerow(row_metabolite + row_concentration)
 
@@ -115,6 +174,22 @@ def main(argv):
                     abnormal_concentrations = []
 
                 elif tag == 'concentration':
+
+                    if 'concentration_value' in concentration:
+                        concentration_value = concentration['concentration_value']
+                        matched = False
+                        for (name, (regex, handler)) in re_handler.items():
+                            m = re.match(regex, concentration_value)
+                            if m:
+                                matched = True
+                                (lower, mean, upper) = handler(m)
+                                concentration[name_concentration_lower] = lower
+                                concentration[name_concentration_middle] = mean
+                                concentration[name_concentration_upper] = upper
+                                break
+                        if not matched:
+                            raise ValueError('Concentration could not be parsed: ' + concentration_value)
+
                     if concentration_abnormal:
                         abnormal_concentrations.append(concentration)
                     else:
@@ -125,6 +200,7 @@ def main(argv):
                         metabolite[tag] = get_value(elem.text)
                     elif tag in attributes_concentration:
                         concentration[tag] = get_value(elem.text)
+
 
 if __name__ == '__main__':
     main(sys.argv)
