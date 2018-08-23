b05172443a5a3160790619ba9b38b59b74b5e1ce
Oliver Alka <randalthor89@gmx.net>
2018-08-23 09:28:30 +0200

    [FIX] bash command not found error due to comments (mac)

--- openms/compress_sirius_output_simple.sh
+++ openms/compress_sirius_output_simple.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-"""
+
+<<COMMENT
 Created on Fri Aug 23
 
 Compress sirius output 
 first argument = output file (e.g. archive.zip)
 second argument = all files that should be zipped (e.g. *)
 
-@author: Oliver Alka 
-"""
+@author: Oliver Alka
+COMMENT
 
 zip -j $1 ${@:2}
